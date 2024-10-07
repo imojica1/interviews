735efe5fb4b84b8e1c018bf8e91ec1a60b7d0460
Irving Mojica <imojica1@corpitsvcs.com>
2024-10-07 12:58:30 -0400

    Add Hosted Service Project

---
+++ DependencyInjection/Worker.cs
@@ -0,0 +1,13 @@
+namespace DependencyInjection
+{
+    public class Worker : BackgroundService
+    {
+        public Worker()
+        {
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+        }
+    }
+}
